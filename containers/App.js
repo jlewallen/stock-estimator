40c92d155e18e89cd8f44d638f796f6018c7a2c4
Jacob Lewallen <jlewallen@gmail.com>
2016-06-07 19:19:06 -0400

    Set layout as default route.

--- containers/App.js
+++ containers/App.js
@@ -12,7 +12,6 @@ class App extends Component {
         const { children } = this.props;
         return (
             <div>
-                <Link to={'/layout'}>Layout</Link>
                 {children}
             </div>
         );
--- routes.js
+++ routes.js
@@ -1,10 +1,11 @@
 import React from 'react';
-import { Route } from 'react-router';
+import { Route, IndexRedirect } from 'react-router';
 import App from './containers/App';
 import LayoutPage from './containers/LayoutPage';
 
 export default (
     <Route path="/" component={App}>
+        <IndexRedirect to="/layout" />
         <Route path="/layout" component={LayoutPage} />
     </Route>
 );
