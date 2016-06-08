3dc3c17d8a85ee428ee35ba388c671acc325cfea
Jacob Lewallen <jlewallen@gmail.com>
2016-06-08 15:01:52 -0700

    Use port from heroku.

--- server.js
+++ server.js
@@ -3,7 +3,7 @@
 import express from 'express';
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 if (process.env.NODE_ENV === 'production') {
     app.get("/static/bundle.js", function (req, res) {
